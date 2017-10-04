bd3b2c4534dc22e9f305e769fd1c11dfe69a8740
Jeffrey C Jones <jjones@fairwaytech.com>
2017-10-03 20:34:51 -0700

    Add output fields.

--- python/launcher.py
+++ python/launcher.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 import tkinter as tk
 
 # Pull settings from csv_settings.py.
@@ -11,28 +13,60 @@ class App:
 
     def __init__(self, master):
 
-        self.setup_go_frame(master)
+        self.setup_main_frame(master)
         self.setup_sftp_frame(master)
         self.setup_art_frame(master)
 
-    def setup_go_frame(self, master):
+    def setup_main_frame(self, master):
 
-        go_frame = tk.LabelFrame(master, text="", padx=20, pady=10)
-        go_frame.grid(row=0, columnspan=2, sticky=tk.W+tk.E, padx=20, pady=10)
+        main_frame = tk.LabelFrame(master, text="Main", padx=10, pady=5)
+        main_frame.grid(row=0, sticky=tk.W+tk.E, padx=10, pady=10)
 
-        tk.Label(go_frame, text="Local filename").grid(row=2, sticky=tk.W)
+        tk.Label(main_frame, text="Local filename").grid(row=10, sticky=tk.W)
         self.localfile = tk.StringVar()
         self.localfile.set(settings.FILENAME)
-        tk.Entry(go_frame, textvariable=self.localfile).grid(row=2, column=1, sticky=tk.W+tk.E)
+        tk.Entry(main_frame, textvariable=self.localfile).grid(row=10, column=1, sticky=tk.W+tk.E)
 
-        tk.Button(go_frame, text="Check File Details", command=self.check_file).grid(row=10)
-        self.check_file_label = tk.Label(go_frame, relief=tk.GROOVE, text="<-- press to get file details", anchor=tk.W)
-        self.check_file_label.grid(row=10, column=1, sticky=tk.W+tk.E)
+        tk.Button(main_frame, text="Check File Details", command=self.check_file).grid(row=20, sticky=tk.W)
+        self.check_file_label = tk.Label(main_frame, relief=tk.GROOVE, text="<-- press to get file details", anchor=tk.W)
+        self.check_file_label.grid(row=20, column=1, sticky=tk.W+tk.E)
+
+        tk.Label(main_frame, text="").grid(row=24, sticky=tk.W)
+        tk.Label(main_frame, text="Downloader Output").grid(row=25, sticky=tk.W)
+        self.downloader_output = tk.Listbox(main_frame, width=100, height=15)
+        self.downloader_output.grid(row=30, column=0, columnspan=2)
+        self.scrollY = tk.Scrollbar(main_frame, orient=tk.VERTICAL, command=self.downloader_output.yview)
+        self.scrollY.grid(row=30, column=2, sticky=tk.N+tk.S)
+        self.scrollX = tk.Scrollbar(main_frame, orient=tk.HORIZONTAL, command=self.downloader_output.xview)
+        self.scrollX.grid(row=40, column=0, columnspan=2, sticky=tk.E+tk.W)
+        self.downloader_output['xscrollcommand'] = self.scrollX.set
+        self.downloader_output['yscrollcommand'] = self.scrollY.set
+        def clear_downloader_output():
+            self.downloader_output.delete(0,self.downloader_output.size())
+        tk.Button(main_frame, text="Clear", command=clear_downloader_output).grid(row=25, column=1, sticky=tk.E)
+
+        tk.Label(main_frame, text="").grid(row=44, sticky=tk.W)
+        tk.Label(main_frame, text="Exporter Output").grid(row=45, sticky=tk.W)
+        self.exporter_output = tk.Listbox(main_frame, width=100, height=15)
+        self.exporter_output.grid(row=50, column=0, columnspan=2)
+        self.scrollY = tk.Scrollbar(main_frame, orient=tk.VERTICAL, command=self.exporter_output.yview)
+        self.scrollY.grid(row=50, column=2, sticky=tk.N+tk.S)
+        self.scrollX = tk.Scrollbar(main_frame, orient=tk.HORIZONTAL, command=self.exporter_output.xview)
+        self.scrollX.grid(row=60, column=0, columnspan=2, sticky=tk.E+tk.W)
+        self.exporter_output['xscrollcommand'] = self.scrollX.set
+        self.exporter_output['yscrollcommand'] = self.scrollY.set
+        def clear_exporter_output():
+            self.exporter_output.delete(0,self.exporter_output.size())
+        tk.Button(main_frame, text="Clear", command=clear_exporter_output).grid(row=45, column=1, sticky=tk.E)
+
+        for x in range(50):
+            self.downloader_output.insert(tk.END, [x*a for a in range(100)])
+            self.exporter_output.insert(tk.END, [x*a for a in range(100)])
 
     def setup_sftp_frame(self, master):
 
-        sftp_frame = tk.LabelFrame(master, text="sFTP Settings", padx=20, pady=10)
-        sftp_frame.grid(row=1, columnspan=2, sticky=tk.W+tk.E, padx=20, pady=10)
+        sftp_frame = tk.LabelFrame(master, text="sFTP Settings", padx=10, pady=5)
+        sftp_frame.grid(row=1, sticky=tk.W+tk.E, padx=10, pady=10)
 
         tk.Label(sftp_frame, text="File Path").grid(row=0, sticky=tk.W)
         self.sftp_file = tk.StringVar()
@@ -61,8 +95,8 @@ class App:
 
     def setup_art_frame(self, master):
 
-        art_frame = tk.LabelFrame(master, text="ART REST API Settings", padx=20, pady=10)
-        art_frame.grid(row=2, columnspan=2, sticky=tk.W+tk.E, padx=20, pady=10)
+        art_frame = tk.LabelFrame(master, text="ART REST API Settings", padx=10, pady=5)
+        art_frame.grid(row=2, sticky=tk.W+tk.E, padx=10, pady=10)
 
         tk.Label(art_frame, text="Endpoint").grid(row=7, sticky=tk.W)
         self.art_endpoint = tk.StringVar()
