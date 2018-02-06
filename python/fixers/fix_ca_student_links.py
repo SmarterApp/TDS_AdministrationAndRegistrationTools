56d0319b3d1b333e7c05a9935f24d199809918c0
Jeffrey C Jones <jjones@fairwaytech.com>
2018-02-05 23:02:25 -0700

    Prepends AND Append zeroes. Add more stats and writes dump files.

--- python/fixers/fix_ca_student_links.py
+++ python/fixers/fix_ca_student_links.py
@@ -2,6 +2,9 @@
 
 # Python 3 required. A virtualenv is recommended. Install requirements.txt.
 
+from enum import Enum
+import datetime
+
 import pymongo
 
 try:
@@ -12,12 +15,30 @@ except:
     print("*** Please copy settings_default.py to settings_secret.py and modify that!")
 
 
+class AutoNumber(Enum):
+    def __new__(cls):
+        value = len(cls.__members__) + 1
+        obj = object.__new__(cls)
+        obj._value_ = value
+        return obj
+
+
+class DistrictEvents(AutoNumber):
+    CACHE_HIT = ()
+    CACHE_MISS = ()
+    FIXED_APPEND_ZEROES = ()
+    FIXED_LINKED_AS_IS = ()
+    FIXED_PREPEND_APPEND_ZEROES = ()
+    NOT_FIXED_DISTRICT_NOT_FOUND = ()
+    NOT_FIXED_FAILED_TO_SAVE = ()
+    NOT_FIXED_NO_DISTRICT_ID = ()
+
+
 class FixerUpper:
 
     def __init__(self):
-        self.districtCache = {}
-        self.cacheHits = 0
-        self.cacheMisses = 0
+        self.district_cache = {}
+        self.district_events = {}
 
         client = pymongo.MongoClient(settings.HOST, settings.PORT)
         self.db = client[settings.DBNAME]
@@ -25,9 +46,22 @@ class FixerUpper:
         self.schools = self.db[settings.INSTITUTION_COLLECTION]
         self.students = self.db[settings.STUDENT_COLLECTION]
 
+    def tally_district_event(self, event):
+        self.district_events[event] = self.district_events.get(event, 0) + 1
+
+    def fail(self, student, event):
+        self.tally_district_event(event)
+        student['fixer_event'] = str(event)
+        self.students_not_fixed.write("%s,%s\n" % (
+            student.get('entityId', None), student.get('districtIdentifier', None)))
+
+    def success(self, student, event):
+        self.tally_district_event(event)
+
     def fix_districts(self):
-        print("\n%d districts, %s schools, %s students in DB:\n\t%s" % (
-            self.districts.count(), self.schools.count(), self.students.count(), self.db))
+        start_time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        print("\nStarted at %s. %d districts, %s schools, %s students in DB:\n\t%s" % (
+            start_time, self.districts.count(), self.schools.count(), self.students.count(), self.db))
 
         cali_no_district = self.students.find({
             "stateAbbreviation": "CA",
@@ -41,72 +75,94 @@ class FixerUpper:
 
         input("\n***** Press enter to start fixing, CTRL-C to cancel! *****")
 
+        self.students_not_fixed = open("students_not_fixed_%s" % start_time, "w")
+
         rowidx = 0
-        notfixed = 0
-        modified = 0
-        districtsNotFound = set()
+        self.districts_not_found = set()
+        self.districts_fixed = set()
 
         for student in cali_no_district:
-
-            # Something nice to look at...
+            # Some nice progress to look at...
             rowidx += 1
             if rowidx % 1000 == 0:
                 print("Processing row %d..." % rowidx)
+            if rowidx > 10000:
+                break
 
-            origDistrictIdentifier = student.get('districtIdentifier', None)
-            if not origDistrictIdentifier:
-                notfixed += 1
+            district_id = student.get('districtIdentifier', None)
+            if not district_id:
+                self.fail(student, DistrictEvents.NOT_FIXED_NO_DISTRICT_ID)
                 continue
 
-            fixed = None
-            # If district ID is 6 characters long, prepend a 0 and try to link.
-            if len(origDistrictIdentifier) == 6:
-                fixed = self.link_district(student, "0%s" % origDistrictIdentifier)
+            failure = None
+            scheme = DistrictEvents.FIXED_APPEND_ZEROES
+            # If district ID is 6 characters long, prepend a 0.
+            if len(district_id) == 6:
+                district_id = "0" + district_id
+                scheme = DistrictEvents.FIXED_PREPEND_APPEND_ZEROES
 
-            # If missing 7 trailing 0's, add them and try to link.
-            if not fixed and not origDistrictIdentifier.endswith("0000000"):
-                fixed = self.link_district(student, "%s0000000" % origDistrictIdentifier)
+            # If missing 7 trailing 0's, add zeroes and link.
+            if not district_id.endswith("0000000"):
+                failure = self.link_district(student, "%s0000000" % district_id)
 
-            # If not fixed yet, just try relinking with district ID as-is.
-            if not fixed:
-                fixed = self.link_district(student, origDistrictIdentifier)
+            # If not fixed yet, try relinking with unmodified district ID.
+            if failure:
+                scheme = DistrictEvents.FIXED_LINKED_AS_IS
+                failure = self.link_district(student, student.get('districtIdentifier'))
 
-            if not fixed:
-                notfixed += 1
-                districtsNotFound.add(origDistrictIdentifier)
-                continue
+            if failure:
+                self.districts_not_found.add(student.get('districtIdentifier'))
+                self.fail(student, failure)
+            else:
+                self.districts_fixed.add(student.get('districtIdentifier'))
+                self.success(student, scheme)
 
-            # Student is fixed! Save record.
-            try:
-                result = self.students.replace_one({'_id': student.get('_id')}, student)
-                modified += 1
-                if result.matched_count != 1:
-                    raise Exception(
-                        "Matched %d documents saving student '%s'! Modified while running?" % result.matched_count)
-            except pymongo.errors.DuplicateKeyError as e:
-                notfixed += 1
-                print("Duplicate key found for entity '%s': %s" % (student, e))
+        self.students_not_fixed.close()
 
         print("Processed %d students." % rowidx)
-        print("Fixed %d students." % modified)
-        print("Could not fix %d students." % notfixed)
-        print("Cached %d districts. %d hits, %d misses." % (len(self.districtCache), self.cacheHits, self.cacheMisses))
-        print("Could not find %d districts, as found on students:\n%s\n" % (len(districtsNotFound), districtsNotFound))
+        print("Cached %d districts." % (len(self.district_cache)))
+        print("Events:")
+        for name, member in DistrictEvents.__members__.items():
+            print("\t%s: %d" % (name, self.district_events.get(member, 0)))
 
-    def link_district(self, student, districtIdentifier):
-        district = None
-        if districtIdentifier in self.districtCache:
-            self.cacheHits += 1
-            district = self.districtCache.get(districtIdentifier)
+        print("Could not find %d districts. Writing..." % len(self.districts_not_found))
+        with open("districts_not_found_%s" % start_time, "w") as f_districts_not_found:
+            for district in sorted(self.districts_not_found):
+                f_districts_not_found.write("%s\n" % district)
+
+        print("Fixed %d districts. Writing..." % len(self.districts_fixed))
+        with open("districts_fixed_%s" % start_time, "w") as f_districts_fixed:
+            for district in sorted(self.districts_fixed):
+                f_districts_fixed.write("%s\n" % district)
+
+    def district_cache_fetch(self, district_identifier):
+        if district_identifier in self.district_cache:
+            self.tally_district_event(DistrictEvents.CACHE_HIT)
+            district = self.district_cache.get(district_identifier)
         else:
-            self.cacheMisses += 1
-            district = self.districts.find_one({"entityId": districtIdentifier})
-            self.districtCache[districtIdentifier] = district  # Cache the district, good or None.
+            self.tally_district_event(DistrictEvents.CACHE_MISS)
+            district = self.districts.find_one({"entityId": district_identifier})
+            self.district_cache[district_identifier] = district  # Cache the district, good or None.
+        return district
+
+    def link_district(self, student, district_identifier):
+        district = self.district_cache_fetch(district_identifier)
         if not district:
-            return None
-        student['districtIdentifier'] = districtIdentifier
+            return DistrictEvents.NOT_FIXED_DISTRICT_NOT_FOUND
+        # District found! Fix student and save record.
+        student['districtIdentifier'] = district_identifier
         student['districtEntityMongoId'] = str(district['_id'])
-        return student
+        return self.save_record(student)
+
+    def save_record(self, student):
+        try:
+            result = self.students.replace_one({'_id': student.get('_id')}, student)
+            if result.matched_count != 1:
+                print("WARN: Matched %d documents saving student '%s'." % (result.matched_count, student))
+        except Exception as e:
+            print("Mongo replace_one() failed. Student '%s'. Exception '%s'. Skipping." % (student, e))
+            return DistrictEvents.NOT_FIXED_FAILED_TO_SAVE
+        return None
 
 
 if __name__ == "__main__":
